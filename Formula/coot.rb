2b312c963b0a166f9636c83dbf6cfef8e391c449
YoshitakaMo <virgospica93@gmail.com>
2022-04-05 18:57:17 +0900

    Coot: for 1.0.01-pre (HEAD)

--- Formula/coot.rb
+++ Formula/coot.rb
@@ -3,23 +3,22 @@ class Coot < Formula
 
   desc "Crystallographic Object-Oriented Toolkit"
   homepage "https://www2.mrc-lmb.cam.ac.uk/personal/pemsley/coot/"
-  url "https://www2.mrc-lmb.cam.ac.uk/personal/pemsley/coot/source/releases/coot-0.9.7.tar.gz"
-  sha256 "f9a612c859ea942c9309d7ebad63b4dffbd3b7ef351e1d007b64cf264317dd28"
+  url "https://www2.mrc-lmb.cam.ac.uk/personal/pemsley/coot/source/releases/coot-1.tar.gz"
+  sha256 "abf7c70651c329ce1db69c7a026bb431af419c8861282cce817dd06eeeb3af99"
   license any_of: ["GPL-3.0-only", "LGPL-3.0-only", "GPL-2.0-or-later"]
 
   head do
-    url "https://github.com/pemsley/coot.git", branch: "refinement"
+    url "https://github.com/pemsley/coot.git", branch: "gtk3"
     depends_on "autoconf" => :build
     depends_on "automake" => :build
     depends_on "libtool" => :build
+    patch :DATA
   end
 
   depends_on "swig" => :build
-  depends_on "wget" => :build
-  depends_on xcode: :build
   depends_on "adwaita-icon-theme" # display icons
-  depends_on "boost@1.74"
-  depends_on "boost-python3@1.74"
+  depends_on "boost"
+  depends_on "boost-python3"
   depends_on "brewsci/bio/clipper4coot"
   depends_on "brewsci/bio/libccp4"
   depends_on "brewsci/bio/mmdb2"
@@ -43,7 +42,6 @@ class Coot < Formula
   depends_on "python@3.9"
   depends_on "rdkit"
   depends_on "readline"
-  # depends_on "qed"
 
   uses_from_macos "curl"
 
@@ -76,19 +74,21 @@ class Coot < Formula
     ENV.append "CPPFLAGS", "-I#{Formula["clipper4coot"].opt_prefix}/fftw2/include"
 
     # Boost root
-    boost_prefix = Formula["boost@1.74"].opt_prefix
-    boost_python_lib = Formula["boost-python3@1.74"].opt_lib
+    boost_prefix = Formula["boost"].opt_prefix
+    boost_python_lib = Formula["boost-python3"].opt_lib
 
     # set RDKit CPPFLAGS (required)
     ENV.append "CPPFLAGS", "-I#{Formula["rdkit"].opt_include}/rdkit"
 
-    # '--with-enhanced-ligand-tools' is not included now due to a compilation failure on lbg.cc
+    # patch to use -std=c++14 for '--with-enhanced-ligand-tools'
+    inreplace "configure.ac", "CXXFLAGS -std=c++11", "CXXFLAGS -std=c++14"
+
     args = %W[
-      SHELL=/bin/bash
       --prefix=#{prefix}
       --with-boost=#{boost_prefix}
       --with-boost-libdir=#{boost_python_lib}
       --with-fftw-prefix=#{fftw2_prefix}
+      --with-enhanced-ligand-tools
     ]
 
     # rdkit_libs is defined, but not used now.
@@ -123,13 +123,9 @@ class Coot < Formula
     args << "RDKIT_LIBS=#{rdkit_libs.join(" ")}"
     args << "RDKIT_CXXFLAGS=#{rdkit_cxxflags.join(" ")}"
 
-    # coot.py is missing in src directory?
-    cd "src" do
-      system "wget", "https://raw.githubusercontent.com/pemsley/coot/master/src/coot.py"
-    end
     system "./configure", *args
     system "make"
-    system "make", "install"
+    ENV.deparallelize { system "make", "install" }
 
     # install reference data
     # install data, #{pkgshare} is /path/to/share/coot
@@ -143,3 +139,60 @@ class Coot < Formula
   end
 end
 
+__END__
+
+diff --git a/utils/backward.hpp b/utils/backward.hpp
+index 195ca8f2d..e661ce7b6 100644
+--- a/utils/backward.hpp
++++ b/utils/backward.hpp
+@@ -4157,7 +4157,11 @@ public:
+ #elif defined(__arm__)
+     error_addr = reinterpret_cast<void *>(uctx->uc_mcontext.arm_pc);
+ #elif defined(__aarch64__)
+-    error_addr = reinterpret_cast<void *>(uctx->uc_mcontext.pc);
++    #if defined(__APPLE__)
++      error_addr = reinterpret_cast<void *>(uctx->uc_mcontext->__ss.__pc);
++    #else
++      error_addr = reinterpret_cast<void *>(uctx->uc_mcontext.pc);
++    #endif
+ #elif defined(__mips__)
+     error_addr = reinterpret_cast<void *>(
+         reinterpret_cast<struct sigcontext *>(&uctx->uc_mcontext)->sc_pc);
+
+
+diff --git a/src/coot.in b/src/coot.in
+index 3b5ef61a0..3db17ea38 100755
+--- a/src/coot.in
++++ b/src/coot.in
+@@ -39,13 +39,15 @@ function check_for_no_graphics {
+ current_exe_dir=$(dirname $0)
+ systype=$(uname)
+
+-if [ $systype = Darwin ] ; then
+-    COOT_PREFIX="$(cd "$(dirname "$current_exe_dir")" 2>/dev/null && pwd)"
+-else
+-    unlinked_exe=$(readlink -f $0)
+-    unlinked_exe_dir=$(dirname $unlinked_exe)
+-    COOT_PREFIX=$(dirname $unlinked_exe_dir)
+-fi
++# ht: https://stackoverflow.com/a/246128
++SOURCE=${BASH_SOURCE[0]}
++while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
++  DIR=$(cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd)
++  SOURCE=$(readlink "$SOURCE")
++  [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
++done
++COOT_BIN_PREFIX=$(cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd)
++COOT_PREFIX=$(dirname $COOT_BIN_PREFIX)
+ # echo COOT_PREFIX is $COOT_PREFIX
+
+
+@@ -147,7 +143,7 @@ export COOT_STANDARD_RESIDUES
+ # export COOT_REFMAC_LIB_DIR
+ export COOT_PYTHON_DIR
+ # export PYTHONPATH
+-export PYTHONHOME
++# export PYTHONHOME
+ export COOT_SCHEME_DIR
+ export COOT_REF_STRUCTS
+ export COOT_RESOURCES_FILE
