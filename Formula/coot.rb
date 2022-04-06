19228f8eae007d94cc21d468700a3759a62fedfc
Blake Riley <blake.riley@gmail.com>
2022-04-05 20:35:55 -0400

    Put glm back in for build

--- Formula/coot.rb
+++ Formula/coot.rb
@@ -14,6 +14,7 @@ class Coot < Formula
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
+  depends_on "glm" => :build
   depends_on "libtool" => :build
   depends_on "pkg-config" => :build
   depends_on "swig" => :build
