6fe23eb24a1a8fb67f0cc44596a5ef99c80cb06a
Blake Riley <4962663+blake-riley@users.noreply.github.com>
2023-12-06 03:14:53 +1100

    Replace venv work with a dependency on python-requests
    I know the venv work is pretty hard-fought, but unless I'm missing something, I think this would be a bit simpler.
    This is only pretty recently available (https://github.com/Homebrew/homebrew-core/pull/155385) / 2 weeks ago.

--- Formula/coot.rb
+++ Formula/coot.rb
@@ -1,5 +1,4 @@
 class Coot < Formula
-  include Language::Python::Virtualenv
   desc "Crystallographic Object-Oriented Toolkit"
   homepage "https://www2.mrc-lmb.cam.ac.uk/personal/pemsley/coot/"
   url "https://www2.mrc-lmb.cam.ac.uk/personal/pemsley/coot/source/releases/coot-1.1.02.tar.gz"
@@ -35,6 +34,7 @@ class Coot < Formula
   depends_on "numpy"
   depends_on "py3cairo"
   depends_on "pygobject3"
+  depends_on "python-requests"
   depends_on "python@3.11"
   depends_on "rdkit"
   depends_on "sqlite"
@@ -51,16 +51,7 @@ class Coot < Formula
     sha256 "03562eec612103a48bd114cfe0d171943e88f94b84610d16d542cda138e5f36b"
   end
 
-  resource "requests" do
-    url "https://files.pythonhosted.org/packages/9d/be/10918a2eac4ae9f02f6cfe6414b7a155ccd8f7f9d4380d62fd5b955065c3/requests-2.31.0.tar.gz"
-    sha256 "942c5a758f98d790eaed1a29cb6eefc7ffb0d1cf7af05c3d2791656dbd6ad1e1"
-  end
-
   patch :DATA
-
-  def python3
-    which("python3.11")
-  end
 
   def install
     ENV.cxx11
@@ -74,11 +65,6 @@ class Coot < Formula
       inreplace "./configure", "$wl-flat_namespace", ""
       inreplace "./configure", "$wl-undefined ${wl}suppress", "-undefined dynamic_lookup"
     end
-
-    # Get Python location
-    xy = Language::Python.major_minor_version python3
-    resource("requests").stage { system python3, *Language::Python.setup_install_args(libexec) }
-    ENV.prepend_path "PYTHONPATH", libexec/"lib/python#{xy}/site-packages"
 
     # Set Boost, RDKit, and FFTW2 root
     boost_prefix = Formula["boost"].opt_prefix
@@ -106,8 +92,6 @@ class Coot < Formula
     # install data, #{pkgshare} is /path/to/share/coot
     (pkgshare/"reference-structures").install resource("reference-structures")
     (pkgshare/"lib/data/monomers").install resource("monomers")
-    # enable python requests
-    (lib/"python#{xy}/site-packages/homebrew-coot.pth").write libexec/"lib/python#{xy}/site-packages"
   end
 
   # test block is not tested now.
